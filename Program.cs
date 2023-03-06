234916b794b9a6aa0650a8ba8c7fed692c71e6b3
plugmxlly <117053595+plugmxlly@users.noreply.github.com>
2023-03-06 11:45:18 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*Задание 1*/
+
+            /*string J = Console.ReadLine();
+            string S = Console.ReadLine();
+
+            var jArray = J.ToCharArray();
+            var sArray = S.ToCharArray();
+
+            int count = 0;
+            
+            for (int i = 0; i < jArray.Length; ++i)
+            {
+                for (int j = 0; j < sArray.Length; ++j)
+                {
+                    if (jArray[i] == sArray[j])
+                    {
+                        ++count;
+                    }
+                }
+            }
+            
+            Console.WriteLine(count);*/
+            
+            /*Задание 2*/
+            
+            /*Задание 3*/
+
+            /*List<int> nums = new List<int>() { 1, 4, 10, 2 };
+            bool isSame = false;
+            for (int i = 0; i < nums.Count; ++i)
+            {
+                for (int j = 0; j < nums.Count; ++j)
+                {
+                    if (i != j)
+                    {
+                        if (nums[i] == nums[j])
+                        {
+                            isSame = true;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(isSame);*/
+        }
+    }
+}
